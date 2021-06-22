6f2b8ca13a0ac753cebe38ea73d2182912506186
Unal <unlklkc@gmail.com>
2021-06-22 21:30:10 +0200

    flask-3 add

---
+++ python/hands-on/flask-03-handling-routes-and-if-for/Flask_if_for_structure/app.py
@@ -0,0 +1,23 @@
+# Import Flask modules
+import re
+from flask import Flask, render_template
+# Create an object named app 
+app = Flask(__name__)
+# Create a function named head which shows the massage as "This is my first conditions experience" in `index.html` 
+# and assign to the route of ('/')
+@app.route('/')
+def head() : 
+    first = "This is my first conditions experience"
+    return render_template('index.html', message = first)
+
+# Create a function named header which prints the items 1 to 10 one by one in `index.html` 
+# and assign to the route of ('/')
+@app.route('/vincenzo')
+def header() :
+    names = ['marcus', 'stephaine', 'marie', 'thomas', 'boyka']
+    return render_template('body.html', object = names)
+
+# run this app in debug mode on your local.
+if __name__ == '__main__' :
+    # app.run(debug = True)
+    app.run(host=‘0.0.0.0’, port=80)
